aeaf67bf220f691218e4436a662651a14129b4d4
Jan Albers <yummiejan@users.noreply.github.com>
2016-12-09 09:12:45 +0100

    mainprogram

--- src/MainProgram.java
+++ src/MainProgram.java
@@ -1,8 +1,22 @@
 import Control.MainController;
+import java.awt.*;
+import View.MainFrame;
 
 /**
  * Created by 204g07 on 09.12.2016.
  */
 public class MainProgram {
-    //ssfdgfdsdg55555555555
+    public static void main (String[] args){
+        EventQueue.invokeLater(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        MainProgram.setup();
+                    }
+                });
+    }
+
+    private static void setup(){
+        new MainController(new MainFrame("Card stack",50,50,800,600));
+    }
 }
