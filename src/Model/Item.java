23fef0faa20515f5d9391f6555f82eda5c47fb0c
H3rb3rg <f-herberg@web.de>
2016-12-09 09:13:46 +0100

    sfgdyf

---
+++ src/Model/Item.java
@@ -0,0 +1,8 @@
+package Model;
+
+/**
+ * Created by 204g04 on 09.12.2016.
+ */
+public abstract class Item {
+
+}
---
+++ src/Model/Player.java
@@ -0,0 +1,7 @@
+package Model;
+
+/**
+ * Created by 204g04 on 09.12.2016.
+ */
+public class Player extends Creature {
+}
