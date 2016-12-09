3e57d66a7ca4201d4a2574b142442350004e3ffa
Jan Albers <yummiejan@users.noreply.github.com>
2016-12-09 08:38:28 +0100

    bisschen gemacht

--- src/Control/MainController.java
+++ src/Control/MainController.java
@@ -1,7 +1,15 @@
 package Control;
 
+import View.*;
+
 /**
  * Created by 204g07 on 09.12.2016.
  */
 public class MainController {
+    private MainFrame frame;
+    private WorldHandler handler;
+
+    public MainController(MainFrame frame){
+        handler = new WorldHandler(frame);
+    }
 }
---
+++ src/Control/WorldHandler.java
@@ -0,0 +1,13 @@
+package Control;
+
+import View.MainFrame;
+
+/**
+ * Created by 204g07 on 09.12.2016.
+ */
+public class WorldHandler {
+
+    public WorldHandler(MainFrame frame){
+
+    }
+}
