765d2b649ac1b284e7429fc035fb14be5fac7d49
H3rb3rg <f-herberg@web.de>
2016-12-12 20:18:48 +0100

    8 - standing infinity!

--- src/Model/Inventory.java
+++ src/Model/Inventory.java
@@ -11,15 +11,21 @@ import java.awt.geom.Rectangle2D;
  */
 public class Inventory implements InteractableObject{
 
+    private Rectangle2D.Double backRectangle;
     private Rectangle2D.Double rectangle;
-    private Rectangle2D place[][];
+    private Rectangle2D.Double rectangle2;
+    private Rectangle2D itemPlace[][];
+    private Rectangle2D amorPlace[];
     private double posX, posY;
 
     public Inventory(double posX, double posY) {
         this.posX = posX;
         this.posY = posY;
+        backRectangle = new Rectangle2D.Double(posX, posY, 10, 10);
         rectangle = new Rectangle2D.Double(posX, posY, 10, 10);
-        place = new Rectangle2D[10][3];
+        rectangle2 = new Rectangle2D.Double(posX, posY, 10, 10);
+        itemPlace = new Rectangle2D[10][4];
+        amorPlace = new Rectangle2D[4];
     }
 
     @Override
@@ -39,12 +45,20 @@ public class Inventory implements InteractableObject{
 
     @Override
     public void draw(DrawingPanel dp, Graphics2D g2d) {
-        for (int i = 0; i < place.length; i++) {
-            for (int j = 0; j < place[i].length ; j++) {
+        g2d.setColor(new Color(79, 79, 79));
+        g2d.fill(backRectangle);
+        backRectangle.setFrame(posX,posY,35*itemPlace.length+55,35*itemPlace[0].length+1);
+        for (int i = 0; i < itemPlace.length; i++) {
+            for (int j = 0; j < itemPlace[i].length ; j++) {
                 g2d.setColor(new Color(0,0,0));
                 g2d.draw(rectangle);
                 rectangle.setFrame(i*35,j*35,35,35);
             }
+        }
+        for (int i = 0; i < amorPlace.length; i++) {
+            g2d.setColor(new Color(0,0,0));
+            g2d.draw(rectangle2);
+            rectangle2.setFrame(35*itemPlace.length+20,i*35,35,35);
         }
     }
 
@@ -52,5 +66,4 @@ public class Inventory implements InteractableObject{
     public void update(double dt) {
 
     }
-
 }
