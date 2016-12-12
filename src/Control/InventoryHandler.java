8db295e1c34a47a60c17e7e9dc33509f9e315192
204g04 <a.steding@hotmail.com>
2016-12-12 10:12:07 +0100

    8 - standing infinity!

---
+++ src/Control/InventoryHandler.java
@@ -0,0 +1,7 @@
+package Control;
+
+/**
+ * Created by 204g04 on 12.12.2016.
+ */
+public class InventoryHandler {
+}
---
+++ src/Model/Inventory.java
@@ -0,0 +1,7 @@
+package Model;
+
+/**
+ * Created by 204g04 on 12.12.2016.
+ */
+public class Inventory {
+}
