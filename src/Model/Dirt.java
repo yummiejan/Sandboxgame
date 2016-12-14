d5b1cb7e005d0bd69a0774ca119f6479a39a36e0
Der Jens <boy1512001@gmail.com>
2016-12-14 12:56:54 +0100

    Farben

--- src/Model/Dirt.java
+++ src/Model/Dirt.java
@@ -5,7 +5,11 @@ package Model;
  */
 public class Dirt extends Block {
 
+    private int randomR = (int)(Math.random() * 40) + 160;
+    private int randomG = (int)(Math.random() * 50) + 80;
+    private int randomB = (int)(Math.random() * 10) + 20;
+
     public Dirt(double posX, double posY) {
-        super(posX, posY, new int[] {84, 42, 25, 0, 0, 0});
+        super(posX, posY, new int[] {(int)(Math.random() * 50) + 160, (int)(Math.random() * 50) + 100, (int)(Math.random() * 50) + 100,0,0,0});
     }
 }
